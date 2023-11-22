e0e0d86c72e2a401c831d28082997705646ce197
Zachary Sain <zachsain@gmail.com>
2023-11-22 15:33:53 -0600

    webhook

---
+++ app/api/whatsapp/route.ts
@@ -0,0 +1,40 @@
+// pages/api/whatsappWebhook.js
+
+import axios from 'axios';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const token = process.env.WHATSAPP_TOKEN;
+const verifyToken = process.env.VERIFY_TOKEN;
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    // Parse the request body from the POST
+    let body = req.body;
+
+    // Check the Incoming webhook message
+    console.log(JSON.stringify(req.body, null, 2));
+
+    // Your existing logic for handling WhatsApp webhook events goes here
+    // ...
+
+    res.status(200).json({ success: true });
+  } else if (req.method === 'GET') {
+    // Handle the verification request
+    const mode = req.query['hub.mode'];
+    const receivedToken = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if (mode && receivedToken) {
+      if (mode === 'subscribe' && receivedToken === verifyToken) {
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      } else {
+        (res as any).sendStatus(403);
+      }
+    } else {
+        (res as any).sendStatus(400);
+    }
+  } else {
+    (res as any).sendStatus(405);; // Method Not Allowed
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -97,15 +97,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+   
         </a>
       </div>
     </main>
